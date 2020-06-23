00df1a8903306f014a32aa56ef932e1640d5e6bf
amankrishna <mnkrishn@gmail.com>
2020-06-23 16:45:39 +0530

    Express + Mongoose

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// connecting with the db server!
+const mongoose = require('mongoose');
+const Dishes = require('../model/dishes');
+
 // dishRuter is now an express router
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
@@ -11,52 +15,78 @@ dishRouter.use(bodyParser.json());
 // I will chain all the verbs
 // i only say slash becuase tthis router is mounted at /dishes
 dishRouter.route('/')
-// here dishes is an endpoint
-.all((req,res,next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    // next(): Look for additional specification down whihc 
-    // will match this endpoint
-    next();
-})
 // if get verb was called at the dishes endpoint
-.get((req,res)=>{
-    res.end('Will send all the dishes to you');
+// then takes two params one to be executed on success and
+// one on failure
+.get((req,res,next)=>{
+    Dishes.find({})
+    .then((dishes)=>{
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(dishes);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
-.post((req,res)=>{
-    // post req will carry some data in JSON form
-    // using app.use(bodyParser.json())
-    // will extract this info as json and apllied to the body of the req
-    res.end('Will add the dish: '+req.body.name+
-        ' with details '+req.body.description);
+.post((req,res,next)=>{
+    // the data to be POSTED is in the body of the request
+    Dishes.create(req.body)
+    // if the dish is successfully added perform this
+    .then((dishes)=>{
+        console.log("Dish Created!");
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(dishes);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
-.put((req,res)=>{
+.put((req,res,next)=>{
     res.statusCode=403;
     res.end("Put operation not supported on dishes");
 })
-.delete((req,res)=>{
-    res.end("Deleting all the dishes");
+.delete((req,res,next)=>{
+    Dishes.remove({})
+    .then((resp)=>{
+        console.log("Dish Created!");
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 });
 
 dishRouter.route('/:dishId')
-.all((req,res,next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    next();
+.get((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dishes)=>{
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(dishes);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
-.get((req,res)=>{
-    res.end("Fetching dish with Id: "+req.params.dishId);
-})
-.post((req, res) => {
+.post((req, res,next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
-.put((req, res) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + 
-          ' with details: ' + req.body.description);
+.put((req, res,next) => {
+    Dishes.findByIdAndUpdate(req.params.dishId,{
+        $set : req.body
+    },{new: true})
+    .then((dish)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
-.delete((req, res) => {
-      res.end('Deleting dish: ' + req.params.dishId);
+.delete((req, res,next) => {
+      Dishes.findByIdAndRemove(req.params.dishId)
+      .then((resp)=>{
+        console.log("Dish Deleted!");
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
   });
 module.exports = dishRouter;
