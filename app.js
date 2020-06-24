59881930f89d3ff8cfb7fa0c194a4dc51fa77fa5
amankrishna <mnkrishn@gmail.com>
2020-06-24 14:19:30 +0530

    Express Session 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -35,12 +37,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // using signed cookie
-app.use(cookieParser('12345-67890-0987-54321'));
+// app.use(cookieParser('12345-67890-0987-54321'));
+
+// set session and define its parameters
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-0987-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 // Authorization Phase
 function auth(req,res,next){
-  console.log(req.signedCookies);
-  if(!req.signedCookies.user){
+  console.log(req.session);
+  if(!req.session.user){
     var authHeader = req.headers.authorization;
 
     if(!authHeader){
@@ -59,7 +70,7 @@ function auth(req,res,next){
 
     if(username==='admin' && password==='password'){
       // set up the cookie on the client side
-      res.cookie('user','admin',{signed: true});
+      req.session.user='admin';
       // move to the next step/middleware
       next();
     }
@@ -72,7 +83,7 @@ function auth(req,res,next){
     }
   }
   else{
-    if(req.signedCookies.user == 'admin'){
+    if(req.session.user === 'admin'){
       next();
     }
     else{
